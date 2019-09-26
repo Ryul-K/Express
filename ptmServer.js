5bea0bb1ab5b659bc33f5dc0bc9c320eca6f4e3b
ryul-k <dyul4440@gmail.com>
2019-09-27 01:55:21 +0900

    dd

--- ptmServer.js
+++ ptmServer.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.get('https://ryul-k.github.io/Express/hello', (req, res) => res.send('Hello World!'))
 
-app.listen(3000, () => console.log('Example app listening on port 3000!'))
+app.get('/index.html', (req, res) => res.send('Hello World!'))
